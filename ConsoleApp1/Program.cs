f70222c66cdc4de904c074e0377df4b5994a185b
buibup <b5209194@gmail.com>
2017-09-03 13:17:35 +0700

    Iniital

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,35 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+[assembly: log4net.Config.XmlConfigurator()]
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static readonly ILog _log = LogManager.GetLogger(typeof(Program));
+        static void Main(string[] args)
+        {
+            log4net.GlobalContext.Properties["AppName"] = Assembly.GetExecutingAssembly().FullName;
+
+
+            _log.Info("App Started");
+
+            try
+            {
+                throw new Exception("A fatal error as occurred.");
+            }
+            catch (Exception ex)
+            {
+                _log.Fatal("Fatal Error: ", ex);
+            }
+
+            Console.WriteLine("Press <Enter> to exit.");
+            Console.ReadLine();
+        }
+    }
+}
